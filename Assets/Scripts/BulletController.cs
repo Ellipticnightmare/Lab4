d0a8d5da6086c4d9366e1362d3a568f344934385
hein <hein@grobler.dev>
2022-03-23 12:47:48 +1100

    Scripts updated
    bullet firing working, scripts updated to reflect object branch

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
     public ActorController thisHolder;
+
+    private void Update()
+    {
+        transform.position += transform.up * Time.deltaTime * 10;
+    }
+
     public void Setup(ActorController actor)
     {
         thisHolder = actor;
+        transform.rotation = thisHolder.transform.rotation;
     }
     public void OnTriggerEnter(Collider other)
     {
